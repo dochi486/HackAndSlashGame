76d8b7676ac7eb3490f8a038e39cfbc9e7f1de8f
dochi486 <93mskim@gmail.com>
2021-07-09 21:43:01 +0900

    시네머신 카메라 삭제
    좌우 이동할 때 카메라까지 회전하는 현상 해결 안돼서 시네머신 사용 안하고 카메라 코드 구현

---
+++ Assets/FollowTargetCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class FollowTargetCamera : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    public BoxCollider moveableArea;
+
+    public float minX, maxX, minZ, maxZ;
+
+
+    void Start()
+    {
+        offset = target.position - transform.position;
+        minX = moveableArea.transform.position.x +  moveableArea.center.x - moveableArea.size.x/2;
+        maxX = moveableArea.transform.position.x +  moveableArea.center.x + moveableArea.size.x/2;
+
+        minZ = moveableArea.transform.position.z +moveableArea.center.z - moveableArea.size.z/2;
+        maxZ = moveableArea.transform.position.z + moveableArea.center.z + moveableArea.size.z/2;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        var newPos = target.position - offset;
+
+        newPos.x = Mathf.Min(newPos.x, maxX);
+        newPos.x = Mathf.Max(newPos.x, minX);
+
+        newPos.z = Mathf.Min(newPos.z, maxZ);
+        newPos.z = Mathf.Max(newPos.z, minZ);
+        transform.position = newPos;
+
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,6 +9,15 @@ public class Player : MonoBehaviour
     public Transform mousePointer;
     public AnimationCurve jumpYac;
     Plane plane = new Plane(new Vector3(0, 1, 0), 0); //두번째 인자 0은 평면을 만드는 노멀의 방향?
+
+    public Transform spriteTr;
+    
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        spriteTr = GetComponentInChildren<SpriteRenderer>().transform; //GetChild해도 자기자신이 우선
+    }
 
     void Update()
     {
@@ -43,14 +53,31 @@ public class Player : MonoBehaviour
         Jump,
         Attack,
     }
-
     StateType state = StateType.Idle;
+    StateType State
+    {
+        get { return state; }
+        set
+        {
+            if (state == value)
+                return;
+            state = value;
+            //AnimationUpdate();
+            animator.Play(state.ToString());
 
+        }
+    }
+
+    //private void AnimationUpdate()
+    //{
+    //    animator.Play(state.ToString());
+    //}
+    Animator animator;
     JumpStateType jumpState;
     IEnumerator JumpCo()
     {
         jumpState = JumpStateType.Jump;
-        state = StateType.Jump;
+        State = StateType.Jump;
         float jumpStartTime = Time.time;
         float jumpDuration = jumpYac[jumpYac.length - 1].time;
         jumpDuration *= jumpTimeMultiply;
@@ -67,7 +94,7 @@ public class Player : MonoBehaviour
         }
 
         jumpState = JumpStateType.Ground;
-        state = StateType.Idle;
+        State = StateType.Idle;
     }
 
     private void Move()
@@ -84,12 +111,28 @@ public class Player : MonoBehaviour
             {
                 var dir = hitPoint - transform.position;
                 dir.Normalize();
-                transform.Translate(dir * speed * Time.deltaTime);
-                state = StateType.Walk;
+                transform.Translate(dir * speed * Time.deltaTime, Space.World);
+
+
+                //방향에 따라 오른쪽은 y가 0 sprite X 45도
+                //왼쪽은... y 180 sprite x -45
+                bool isRightSide = dir.x > 0;
+                if(isRightSide)
+                {
+                    transform.rotation = Quaternion.Euler(Vector3.zero);
+                    spriteTr.rotation = Quaternion.Euler(45, 0, 0);
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(Vector3.zero);
+                    spriteTr.rotation = Quaternion.Euler(-45, 180, 0); //부모의 로테이션이 변경되어서 로컬 y축 값도 180으로 변경해야되더라..?
+                }
+
+                State = StateType.Walk;
             }
             else
             {
-                state = StateType.Idle;
+                State = StateType.Idle;
             }
         }
     }
