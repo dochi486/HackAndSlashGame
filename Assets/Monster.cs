632d426a1948d0dcfa0bbdd72f314054b2db32d3
dochi486 <93mskim@gmail.com>
2021-07-14 20:18:36 +0900

    1. Monster 스크립트의 함수들을 자식이 사용 가능하도록 접근한정자  protected로 변경 2. Skeleton이 50%의 확률로 방패모션, 공격 블락하도록 구현

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -26,7 +26,7 @@ public class Monster : MonoBehaviour
     }
 
     Func<IEnumerator> m_currentFSM;
-    Func<IEnumerator> CurrentFSM
+    protected Func<IEnumerator> CurrentFSM
     {
         get { return m_currentFSM; }
         set
@@ -36,7 +36,7 @@ public class Monster : MonoBehaviour
         }
     }
     //반환형이 IEnumerator로 있기 때문에 action을 사용할 수 없음.. Function을 써야한다.
-    Player player;
+    protected Player player;
     public float detectRange = 20;
     public float attackRange = 10;
 
@@ -58,7 +58,7 @@ public class Monster : MonoBehaviour
     }
 
     public float speed = 34;
-    IEnumerator ChaseFSM()
+    protected IEnumerator ChaseFSM()
     {
         PlayAnimation("Run");
         //yield return null;
@@ -80,7 +80,8 @@ public class Monster : MonoBehaviour
 
             if ((Vector3.Distance(transform.position, player.transform.position) < attackRange))
             {
-                CurrentFSM = AttackFSM;
+                SelectAttackType();
+
                 yield break;
                 //실행하고 있는 코루틴을 빠져나가는 문장
                 //나가면 다음 코루틴 지정한 곳(공격)으로 나간다?
@@ -88,10 +89,18 @@ public class Monster : MonoBehaviour
             yield return null;
         }
     }
+
+    virtual protected void SelectAttackType()
+    {
+        CurrentFSM = AttackFSM;
+        //공격 타입을 여러가지로 지정해서 스켈레톤이 방패모션을 랜덤하게 할 예정
+        //virtual함수로 구현 -> virtual함수는 private이 안되고 protected로 자식이 사용가능하게 접근한정자
+    }
+
     public float attackTime = 1;//공격하고 있는 시간
     public float attackApplyTime = 0.3f; //공격이 실제로 적용되는? 감지되는 시간!
     public int power = 10;
-    IEnumerator AttackFSM()
+    protected IEnumerator AttackFSM()
     {
         PlayAnimation("Attack");
         yield return new WaitForSeconds(attackApplyTime);
@@ -103,14 +112,15 @@ public class Monster : MonoBehaviour
         CurrentFSM = ChaseFSM;
     }
     public float hp = 100;
-    internal void TakeHit(float damage)
+    virtual public void TakeHit(float damage)
     {
         hp -= damage;
         StopCoroutine(fsmHandle);
-
         CurrentFSM = TakeHitFSM;
     }
+
     public float takeHitTime = 0.3f;
+
     private IEnumerator TakeHitFSM()
     {
         PlayAnimation("Hit");
@@ -140,7 +150,7 @@ public class Monster : MonoBehaviour
     //    Destroy(gameObject);
     //}
 
-    void PlayAnimation(string clipName)
+    protected void PlayAnimation(string clipName)
     {
         //Debug.Log(clipName);
         animator.Play(clipName, 0, 0);
--- Assets/Skeleton.cs
+++ Assets/Skeleton.cs
@@ -1,8 +1,45 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Skeleton : Monster
 {
+    //방패 막기 추가 + 공격 타이밍에 랜덤하게 방패 모션
+    //방패 모션 중에는 대미지 X 
+    //막았다는 이펙트 생성
 
+    override protected void SelectAttackType()
+    {
+        if (Random.Range(0, 1f) > 0.5f) //Random.Range(0,1)은 결과가 항상 0이 나오지만 1f로 하면 0~1까지 소수점 범위 사용 가능
+            CurrentFSM = AttackFSM;
+        else
+            CurrentFSM = ShieldFSM;
+    }
+    //override는 ToString으로 많이 사용했었다! 
+
+    bool isDefending = false;
+    public float activeDefendingTime = 2;
+    protected IEnumerator ShieldFSM()
+    {
+        PlayAnimation("Shield");
+        isDefending = true;
+
+        yield return new WaitForSeconds(activeDefendingTime);
+
+        isDefending = false;
+        CurrentFSM = ChaseFSM;
+    }
+
+    public GameObject blockEffect;
+    public override void TakeHit(float damage)
+    {
+        if (isDefending)
+        {
+            Instantiate(blockEffect, transform.position, Quaternion.identity); //자기 위치에 로테이션 0인 값으로 생성하는 일반적인 코드
+        }
+        else
+        {
+            base.TakeHit(damage);
+        }
+    }
 }
