dfb2b8e7a22bad48f117e991339440e30b7bfe9a
dochi486 <93mskim@gmail.com>
2021-07-14 19:05:35 +0900

    타이틀씬
    페이드인으로 타이틀씬 나오고 버튼 누르면 페이드인 페이드아웃

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -1,31 +1,34 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class TitleManager : MonoBehaviour
 {
     CanvasGroup blackScreen;
+    Button button;
     // Start is called before the first frame update
     void Start()
     {
-      
+
         blackScreen = GameObject.Find("PersistCanvas").transform.Find("BlackScreen").GetComponent<CanvasGroup>();
         //검은 화면에서 페이드인으로 시작
 
-        blackScreen.DOFade(0, 0.7f);
+        blackScreen.gameObject.SetActive(true);
+        blackScreen.alpha = 1;
+        blackScreen.DOFade(0, 1.5f).OnComplete(() => blackScreen.gameObject.SetActive(false));
 
         //뉴게임 버튼 누르면 스테이지 1로드
         //검은화면에서 페이드아웃되면서 스테이지1 페이드인 
-        Button button = GameObject.Find("TitleCanvas").transform.Find("Button").GetComponent<Button>();
+        button = GameObject.Find("TitleCanvas").transform.Find("Button").GetComponent<Button>();
 
         button.AddListener(this, OnClickNewGame);
     }
-public void OnClickNewGame()
+    public void OnClickNewGame()
     {
-        blackScreen.DOFade(1, 0.7f).OnComplete(()=> 
+        blackScreen.gameObject.SetActive(true);
+        blackScreen.DOFade(1, 0.7f).OnComplete(() =>
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("Stage1");
-        
         });
         //알파의 최대값은 1이고 어두운 화면을 보이게 한다...
 
