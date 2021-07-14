501fdbcc6bc7e5c5e4313ce428ffd84031b55148
dochi486 <93mskim@gmail.com>
2021-07-14 18:25:40 +0900

    타이틀씬
    1. 시작할 때 검정화면 페이드아웃
    2. 메인 씬으로 넘어갈 때 페이드인 페이드아웃

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class TitleManager : MonoBehaviour
 {
+    CanvasGroup blackScreen;
     // Start is called before the first frame update
     void Start()
     {
+      
+        blackScreen = GameObject.Find("PersistCanvas").transform.Find("BlackScreen").GetComponent<CanvasGroup>();
         //검은 화면에서 페이드인으로 시작
+
+        blackScreen.DOFade(0, 0.7f);
 
         //뉴게임 버튼 누르면 스테이지 1로드
         //검은화면에서 페이드아웃되면서 스테이지1 페이드인 
+        Button button = GameObject.Find("TitleCanvas").transform.Find("Button").GetComponent<Button>();
 
+        button.AddListener(this, OnClickNewGame);
     }
-
-    // Update is called once per frame
-    void Update()
+public void OnClickNewGame()
     {
+        blackScreen.DOFade(1, 0.7f).OnComplete(()=> 
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Stage1");
+        
+        });
+        //알파의 최대값은 1이고 어두운 화면을 보이게 한다...
 
     }
 }
