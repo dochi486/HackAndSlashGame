60248c971fb3cc10f697fae4cef3bf952b53712f
dochi486 <93mskim@gmail.com>
2021-07-15 20:57:14 +0900

    GameManager BaseUI클래스 상속해서 더 쓰기 쉬운 싱글턴으로 만들었따
    원래 있는 ㄱ클래스가 아니라 만든 클래스

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : BaseUI<GameManager>
+
+{
+    //public static GameManager instance;
+
+    //private void Awake()
+    //{
+    //    if(instance != null)
+    //    {
+    //        Debug.LogWarning("instance != null");
+    //        Destroy(gameObject);
+    //        return;
+    //    }
+    //    instance = this;
+
+    //    //DontDestroyOnLoad(gameObject);
+    //}
+
+    //private void OnDestroy()
+    //{
+    //    instance = null;  
+    //    //Destroy될 때 초기화 하기 때문에 instance는 무조건 1개
+    //}
+}
