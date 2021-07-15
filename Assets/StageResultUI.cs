fcd827e520fa24d5319e9e90f769f0028bb9e14d
dochi486 <93mskim@gmail.com>
2021-07-15 21:16:20 +0900

    StageResultUI 배치 및 스크립트 생성

---
+++ Assets/StageResultUI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine.UI;
+
+public class StageResultUI : BaseUI<StageResultUI>
+{
+    Text gradeText;
+    Text eneiesKilledText;
+    Text damageTakenText;
+    Button continueButton;
+
+    void Start()
+    {
+
+        gradeText = transform.Find("StageResultUI/GradeText").GetComponent<Text>();
+        eneiesKilledText = transform.Find("StageResultUI/EneiesKilledText").GetComponent<Text>();
+        damageTakenText = transform.Find("StageResultUI/DamageTakenText").GetComponent<Text>();
+        continueButton = transform.Find("StageResultUI/ContinueButton").GetComponent<Button>();
+        continueButton.AddListener(this, LoadNextStage);
+    }
+
+    private void LoadNextStage()
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
