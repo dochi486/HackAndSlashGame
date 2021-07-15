52d3b17b8d08dc0e22f026d11d6efba548550fd9
dochi486 <93mskim@gmail.com>
2021-07-15 21:31:58 +0900

    Player가 아닌 몬스터의 수를 카운트하는 내용 추가

--- Assets/StageManager.cs
+++ Assets/StageManager.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using DG.Tweening;
+﻿using DG.Tweening;
 using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 /// <summary>
 /// 스테이지에서 발생하는 모든 이벤트를 관리하고 스테이지가 끝나면 같이 파괴된다.
@@ -15,10 +16,19 @@ public class StageManager : MonoBehaviour
     public static StageManager instance;
     public GameStateType gameState = GameStateType.Ready;
 
+    public int sumMonsterCount;
+    public int enemiesKilledCount;
+    public int damageTaken;
+
     private void Awake()
     {
         instance = this;
         gameState = GameStateType.Ready;
+
+        List<SpawnPoint> allSpawnPoints = new List<SpawnPoint>(FindObjectsOfType<SpawnPoint>());
+        //FindObjects의 결과 배열을 그대로 리스트에 담는 내용
+        sumMonsterCount = allSpawnPoints.Where(x => x.spawnType != SpawnType.Player).Count();
+        //리스트로 받았기 때문에 타입에 따른 분류를 Linq로 쉽게 할 수 있다!
     }
     private void OnDestroy()
     {
@@ -39,7 +49,7 @@ public class StageManager : MonoBehaviour
         //OnComplete람다식으로 쉬지 않는 이유는 쉬어야하는 부분이 많기 때문에 람다식보다 코루틴으로 쉬는 게 더 직관적이다. 
 
         //스테이지 이름 표시 "Stage 1"
-     
+
         StageInfo stageInfo = GameData.stageInfoMap[SceneProperty.instance.StageID];
         //GameData.instance.stageInfoMap[SceneProperty.instance.StageID];
         string stageName = stageInfo.titleString;
