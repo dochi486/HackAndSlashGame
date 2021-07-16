ce5df41aeba8c210adf5ef838ebbd9967eba90f3
dochi486 <93mskim@gmail.com>
2021-07-16 22:54:02 +0900

    StageCanvas 앵커 수정
    사이즈가 좀 작긴한데 일단 맞춘 것으로 만족

--- Assets/SceneProperty.cs
+++ Assets/SceneProperty.cs
@@ -25,8 +25,9 @@ public class SceneProperty : MonoBehaviour
     }
     private void OnDestroy()
     {
-        instance = null;  
+        instance = null;
         //꼭 해줘야하는 것? 의도치 않은 결과를 막위 해서 싱글턴을 사용할 때 디스트로이에서 null로 초기화 해줘야한다. 
         //null 초기화를 안하게 되면: 
+
     }
 }
